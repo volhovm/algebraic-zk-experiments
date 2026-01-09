61217a66631245084c75c2654803bbb79f0057a8
Misha Volkhov <mv@volhovm.com>
2026-01-09 15:11:46 +0000

    Fix warning

--- zkbrownian-rs/src/proving/bulletproofs/r1cs/verifier.rs
+++ zkbrownian-rs/src/proving/bulletproofs/r1cs/verifier.rs
@@ -660,13 +660,13 @@ impl<T: BorrowMut<Transcript>, C: AffineRepr> Verifier<T, C> {
         // homomorphically evaluate t polynomial at x
         let mut T_points = vec![];
         let mut T_scalars = vec![];
-        for (d, rx) in rxs.iter().enumerate().take(t_poly_deg + 1) {
+        for (d, _rx) in rxs.iter().enumerate().take(t_poly_deg + 1) {
             if d == op_degree {
                 continue;
             }
             #[cfg(debug_assertions)]
             {
-                println!("T[{}]: {} {}", d, proof.T[d].clone(), rx);
+                println!("T[{}]: {} {}", d, proof.T[d].clone(), _rx);
             }
             T_points.push(proof.T[d]);
             T_scalars.push(rxs[d]);
